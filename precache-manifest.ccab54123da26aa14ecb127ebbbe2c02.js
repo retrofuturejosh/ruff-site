768ed739dd1737516d7bb08e0841e8a22e129011
Retro Future Music <JoshuaSohn@joshuas-mac-pro.home>
2020-05-13 14:40:31 -0400

    Updates

---
+++ precache-manifest.ccab54123da26aa14ecb127ebbbe2c02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ee692cb53b3426ee93fcb5f71f55da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76173c02194af8d393f9",
+    "url": "/static/css/main.d3c45a0b.chunk.css"
+  },
+  {
+    "revision": "27464d3acf2a791891ee",
+    "url": "/static/js/2.1489d5dc.chunk.js"
+  },
+  {
+    "revision": "40158217dcba29c41bf6c1afe3c208d6",
+    "url": "/static/js/2.1489d5dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76173c02194af8d393f9",
+    "url": "/static/js/main.d6279c19.chunk.js"
+  },
+  {
+    "revision": "d5aa3e3a8ad24d8f45a8",
+    "url": "/static/js/runtime-main.a85c9dbe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
