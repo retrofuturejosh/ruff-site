d107b0e55922b6967281c437534c90aff39baa55
Retro Future Music <JoshuaSohn@joshuas-mac-pro.home>
2020-09-13 00:37:07 -0400

    updates links

--- src/mappers/linkMapper.js
+++ src/mappers/linkMapper.js
@@ -1,10 +1,10 @@
 const linkMap = {
     strangers: {
-        spotify: "http://spotify.com",
-        apple: "applemusic.com",
-        tidal: 'tidal.com',
-        google: 'google.com',
-        amazon: 'amazon.com'
+        spotify: "https://open.spotify.com/album/3eh4VVaMBjGQwQTeRq6Ovu?si=aJO33NVnTbGLa-3iqSapug",
+        apple: "https://music.apple.com/us/album/strangers-in-the-dark-single/1530525376",
+        tidal: 'https://tidal.com/browse/album/154209840',
+        google: 'https://play.google.com/store/music/album/Ruff_Strangers_in_the_Dark?id=Bxvum7hhsuymdxmbav26podleeq&hl=en_US',
+        amazon: 'https://music.amazon.com/albums/B08HKT9252?trackAsin=B08HKS1NLL&ref=dm_sh_73c3-b95d-dmcp-b60e-9df76&musicTerritory=US&marketplaceId=ATVPDKIKX0DER'
     }
 }
 
